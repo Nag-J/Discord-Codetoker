c88b18f39f3b6c2b38773a945b5eee116b50d740
Nag-J <junngt1106@gmail.com>
2019-11-13 01:41:54 +0900

    [fix]fixで表せないレベルの大幅変更と機能修正

--- start_bot.py
+++ start_bot.py
@@ -5,93 +5,57 @@ import requests
 import os
 import sys
 import time
+import asyncio
+import traceback
+
+INITIAL_EXTENSIONS = [
+    'cogs.codetokercog'
+]
 
 TOKEN = sys.argv[1]
 ACTIVE_CHANNEL = sys.argv[2]
 VTEXT_KEY = sys.argv[3]
 
-bot = commands.Bot(command_prefix='>')
 talker = 'hikari'
 lines = []
 flagvt = False
 
-@bot.command()
-async def join(ctx):
-    print('join')
-    if ctx.voice_client is None:
-        if ctx.author.voice:
-            await ctx.author.voice.channel.connect()
-            VoiceText(ctx.voice_client, 'CodeTokerが参加しました')
-        else:
-            await ctx.send("You are not connected to a voice channel.")
+class Codetoker(commands.Bot):
+    def __init__(self, command_prefix):
+        super().__init__(command_prefix)
+        self.talker = 'hikari'
+        self.lines = []
+        self.task = None
 
-@bot.command()
-async def bye(ctx):
-    print('leave')
-    await ctx.voice_client.disconnect()
+        for cog in INITIAL_EXTENSIONS:
+            try:
+                self.load_extension(cog)
+            except Exception:
+                traceback.print_exc()
 
-@bot.command()
-async def stop(ctx):
-    if ctx.voice_client.is_playing():
-        ctx.voice_client.stop()
+    async def on_ready(self):
+        print('ready!')
 
-@bot.command()
-async def server_stop(ctx):
-    if ctx.voice_client:
-        ctx.voice_client.disconnect()
-    sys.exit()
+    async def on_message(self, message):
+        print('on_message')
+        if message.author.bot:
+            pass
+        elif message.content.startswith('>'):
+            await self.process_commands(message)
+        elif message.channel.id == int(ACTIVE_CHANNEL):
+            if message.guild.voice_client.is_connected():
+                if self.task is None or self.task.done():
+                    self.task = asyncio.create_task( self.speak(message.guild.voice_client, message.content) )
+                else:
+                    self.lines.append(message.content)
 
-@bot.command()
-async def change(ctx, message):
-    global talker
-    if message == 'hikari':
-        talker = 'hikari'
-    elif message == 'takeru':
-        talker = 'takeru'
-    elif message == 'show':
-        talker = 'show'
-    elif message == 'haruka':
-        talker = 'haruka'
-    else:
-        VoiceText(ctx.voice_client, message + 'という声はありません')
-        return
-    VoiceText(ctx.voice_client, message + 'の声に変わりました')
-
-@bot.event
-async def on_ready():
-    print('ready!')
-
-@bot.event
-async def on_message(message):
-    print('on_message')
-    if message.author.bot:
-        pass
-    elif message.content.startswith('>'):
-        await bot.process_commands(message)
-    elif message.channel.id == int(ACTIVE_CHANNEL):
-        if message.guild.voice_client:
-            VoiceText(message.guild.voice_client, message.content)
-
-def VoiceText(voice_client, message=None):
-    global talker
-    global lines
-    global flagvt
-    if flagvt is True:
-        print('stack')
-        lines.append(message)
-    elif message is None:
-        if lines:
-            print('pop')
-            VoiceText(voice_client, lines.pop())
-    else:
-        flagvt = True
-        print('speak')
+    async def speak(self, voice_client, message):
         data = {
             'text': message,
-            'speaker': talker
+            'speaker': self.talker
         }
 
-        response = requests.post('https://api.voicetext.jp/v1/tts', data=data, auth=(VTEXT_KEY, ''))
+        response = requests.post('https://api.VoiceText.jp/v1/tts', data=data, auth=(VTEXT_KEY, ''))
         f = open("vtext.wav", 'wb')
         f.write(response.content)
         f.close()
@@ -99,8 +63,9 @@ def VoiceText(voice_client, message=None):
         voice_client.play(source)
         print('played')
         while voice_client.is_playing():
-            time.sleep(0.5)
-        flagvt = False
-        VoiceText(voice_client)
-
+            await asyncio.sleep(0.3)
+        if self.lines:
+            await self.speak(voice_client, self.lines.pop())
+                
+bot = Codetoker(command_prefix='>')
 bot.run(TOKEN)
